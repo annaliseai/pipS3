9fb8cf5d4b47cbc205d98c0cf251b0dc886f5956
Carl Hattingh <carl.hattingh@annalise.ai>
2020-03-13 10:59:32 +1100

    pipeline update

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-# Build wheel 
+# Build wheel
 
-set -eo
+set -euo pipefail
 
 export LANG=en_AU.utf8
 
 /opt/python/cp37-cp37m/bin/python setup.py sdist
 
-/opt/python/cp37-cp37m/bin/pip install dist/*.whl
+/opt/python/cp37-cp37m/bin/pip install dist/*.tar.gz
 
 s3pkgup
