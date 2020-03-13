55ec34002c398c3f36602254c393304d152939d9
Ben Johnston <ben.johnston@harrison.ai>
2020-03-13 14:31:07 +1100

    build as wheel

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 
 export LANG=en_AU.utf8
 
-/opt/python/cp37-cp37m/bin/python setup.py sdist
+/opt/python/cp37-cp37m/bin/python setup.py bdist_wheel 
 
 /opt/python/cp37-cp37m/bin/pip install dist/*.tar.gz
 
